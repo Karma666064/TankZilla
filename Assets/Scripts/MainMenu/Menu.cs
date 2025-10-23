234eb8ffda1d6a0fffa70a89718cb7423ca4768c
Karma <72810712+Karma666064@users.noreply.github.com>
2025-10-23 13:01:23 +0400

    save

---
+++ Assets/Scripts/MainMenu/Menu.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    [SerializeField] GameObject playPannel;
+    [SerializeField] GameObject settingPannel;
+
+    Vector3 initialPosPanel = new Vector3(-637.1f, 26.1f, 0);
+    Vector3 targetPosPanel = new Vector3(1162.9f, 26.1f, 0);
+
+    void OpenPlayPanel()
+    {
+
+    }
+}
